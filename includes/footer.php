4db09de2598506aa2d588ab473a0a4f1acdaebd7
WorthyAA <adr254@internet.ru>
2025-06-03 18:36:48 +0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,13 @@
 <footer class="footer mt-auto py-3 bg-light shadow-sm">
     <div class="container text-center">
         <span class="text-muted">
-            <a href="https://github.com/Joulerk/WorldDomination" class="text-muted" target="_blank">
-                <i class="material-icons">public</i> Мировое Господство | ЛЕРК &copy; 2024
+            <a href="185.233.186.89" class="text-muted" target="_blank">
+                <i class="material-icons">public</i> Мировое Господство&copy; 2024
             </a>
         </span>
         <span class="text-muted ml-3">
-            <a href="https://boosty.to/lerk" class="text-muted" target="_blank">
-                <i class="material-icons">attach_money</i> Boosty
+            <a href="185.233.186.89" class="text-muted" target="_blank">
+                <i class="material-icons">attach_money</i>
             </a>
         </span>
     </div>
